f5af39a6c3b605dc4d06681173419e0daef4e433
Daniel Rubio <drubio1989@gmail.com>
2019-02-06 16:16:29 -0600

    Lesson Two: VrButton, State, Dynamic Changes

--- HouseTourVR/index.js
+++ HouseTourVR/index.js
@@ -1,23 +1,69 @@
 import React from 'react';
 import {
+  asset,
   AppRegistry,
+  Environment,
   StyleSheet,
   Text,
   View,
+  VrButton
 } from 'react-360';
 import house from './data/houseData';
 
 export default class HouseTourVR extends React.Component {
+  state = {
+    room: '',
+    info: '',
+    adjacentRooms: [],
+    img: ''
+  }
+
+  componentWillMount() {
+    if (this.state.room === '') {
+      this.setState({
+        room: house.House.roomName,
+        info: house.House.info,
+        adjacentRooms: house.House.adjacentRooms
+      })
+    }
+  }
+
+  clickHandler(roomSelection) {
+
+    this.setState({
+      room: house[`${roomSelection}`].roomName,
+      info: house[`${roomSelection}`].info,
+      adjacentRooms: house[`${roomSelection}`].adjacentRooms
+    })
+
+    Environment.setBackgroundImage(asset(`./360_${house[`${roomSelection}`].img}`));
+  }
+
+  createRoomButtons(adjacentRooms) {
+    let rooms = adjacentRooms;
+    let buttons = [];
+
+    rooms.map(room => (
+      buttons.push(
+        <VrButton key={`${room}` + '-button'} onClick={() => this.clickHandler(room)}>
+          <Text style={{backgroundColor: 'green'}}>{ room }</Text>
+        </VrButton>)
+    ));
+
+    return buttons;
+  }
+
   render() {
     return (
       <View style={styles.panel}>
         <View style={styles.greetingBox}>
           <Text>Room Selection</Text>
-          <Text>{ house.House.roomName }</Text>
+          <Text>{ this.state.room }</Text>
+          { this.createRoomButtons(house[this.state.room.replace(/\s/g, '')].adjacentRooms) }
         </View>
         <View style={styles.greetingBox}>
           <Text>Room Info</Text>
-          <Text>{ house.House.info }</Text>
+          <Text>{ this.state.info}</Text>
         </View>
       </View>
     );
