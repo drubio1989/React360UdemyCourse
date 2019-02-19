e9564429ca2a2b6073a01e8d3bf080c343263ceb
Daniel Rubio <drubio1989@gmail.com>
2019-02-18 20:01:00 -0600

    Lesson One: Adding 3D Objects

---
+++ CryptoDashBoardVR/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  asset,
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-360';
+import Entity from 'Entity';
+
+export default class CryptoModel extends React.Component {
+  render() {
+    return (
+      <View>
+        <Entity
+          style={{transform: [{scaleX: 1}, {scaleY: 1}, {scaleZ: 1}, {rotateX: 90}]}}
+          source={{obj: asset('models/BTC.obj')}}
+        />
+      </View>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+});
+
+AppRegistry.registerComponent('CryptoModel', () => CryptoModel);
